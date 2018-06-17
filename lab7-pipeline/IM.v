4d35f494744e6f226296cec0963376b5760206bb
mark14wu <mark14@qq.com>
2018-06-17 14:10:52 +0800

    removed junk comments

--- lab7-pipeline/IM.v
+++ lab7-pipeline/IM.v
@@ -3,12 +3,7 @@ module IM(A,IR);
     output [31:0] IR;     // 32-bit memory output
     reg[31:0] im_128k[0:32767];  // 1024  * 32bit =    4KB
     // 32768 * 32bit =  128KB 
+    
+    assign IR = im_128k[A[15:2]]; // 右移两位
 
-    initial begin
-        // $readmemh("./code/code_wkx_2.txt",im_128k,32'H0C00,32'H0C0e);
-        // $readmemh("code_rz.txt",im_4k,32'H0C00,32'H0C1D); 
-        // $readmemh("code_hazard.txt",im_4k,32'H0C00,32'H0C05);
-    end
-
-    assign IR = im_128k[A[15:2]];//number addr[9:0] instruction
 endmodule

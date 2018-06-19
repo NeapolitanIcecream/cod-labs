0aa92d060cd0b51a9adc3be970158a5cdef7833b
mark14wu <mark14@qq.com>
2018-06-20 02:05:45 +0800

    new fib implementation

--- lab1-alu/fib.v
+++ lab1-alu/fib.v
@@ -1,12 +1,42 @@
 module fib(
            input [31:0]  a,
-           output [31:0] fib
-           );
+           input clk, rst_n,
+           output [31:0] fib,
+           output flag);
    wire [31:0]           w1, w2, w3;
-   parameter op = 5'h1;
+   reg [31:0]            r1, r2, r3;
+   reg                   flag;
 
-   alu myalu1(a, a, op, w1);
-   alu myalu2(a, w1, op, w2);
-   alu myalu3(w1, w2, op, w3);
-   alu myalu4(w2, w3, op, fib);
+   parameter n = 4;
+   parameter op = 5'h1;
+   integer count = 0;
+
+   //alu myalu3(r1, r2, op, r3);
+   alu myalu2(w1, w2, op, w3);
+   assign w1 = r1;
+   assign w2 = r2;
+   assign fib = w3;
+
+   always@(posedge clk or negedge rst_n)
+     begin
+        if(~rst_n)
+          begin
+             flag = 0;
+             count = 0;
+             r1 <= a;
+             r2 <= a;
+          end
+        if (count < n)
+          begin
+             r3 = w3;
+             r1 = r2;
+             r2 = r3;
+             count = count + 1;
+          end
+        else
+          begin
+             flag = 1;
+          end
+     end // always@ (posedge clk or negedge rst_n)
+   
 endmodule // fib
--- lab1-alu/tb.v
+++ lab1-alu/tb.v
@@ -1,35 +1,82 @@
-module alu_testbench;
-   //integer [31:0] a, b;
-   //integer        a, b, op;
-   //reg [31:0] out;
-   //reg [4:0]  op;
+module tb;
    reg [31:0] a, b;
-   reg [4:0] op;
+   reg [4:0]  op;
+   reg       clk, rst_n;
    wire [31:0] out;
    wire [31:0] fib;
 
    alu myalu(a, b, op, out);
+
+   wire fib_finish;
+   integer alu_finish;
+   integer init_finish = 0;
+
+   integer state;
+   integer clk_start;
+
    initial
      begin
-        $dumpfile("testbench.vcd");
+        // initializing wave files
+        $dumpfile("tb.vcd");
         $dumpvars;
+        // initializing alu
+        alu_finish = 0;
         a = 32'h3;
         b = 32'h2;
         op = 5'h0;
-        #100;
-        op = 5'h1;
-        #100;
-        op = 5'h2;
-        #100;
-        op = 5'h3;
-        #100;
-        op = 5'h4;
-        #100;
-        op = 5'h5;
-        #100;
-        op = 5'h6;
-        #100;
-   end // initial begin
-   fib myfib(b, fib);
-endmodule // alu_testbench
+        state = 0;
+        // initializing clk
+        clk = 0;
+        clk_start = 0;
+        // initializing fib
+        rst_n = 1;
+        #5;
+        rst_n = 0;
+        #5;
+        rst_n = 1;
+        #5;
+        // initialization finished
+        init_finish = 1;
+     end // initial begin
+
+   always@(*)
+     begin
+        if (init_finish)
+          begin
+             if (fib_finish & alu_finish)
+               begin
+                  $finish;
+               end
+             else
+               begin
+                  clk_start = 1;
+               end
+          end // if (init_finish)
+     end // always@ (*)
+
+   always #10 if(clk_start) clk = ~clk;
+
+   always@(posedge clk)
+     begin
+        if (op != 5'h7)
+          begin
+             if (state == 0)
+               begin
+                  state = 1;
+               end
+             else
+               begin
+                  op = op + 1;
+                  state = 0;
+               end
+          end // if (op != 5'h7)
+        else
+          begin
+             alu_finish = 1;
+          end // else: !if(op != 5'h7)
+     end // always@ (posedge clk)
+
+   fib myfib(a, clk, rst_n, fib, fib_finish);
+
+endmodule // tb
 
